4307656877bcf45dd367538517a9b1ccbae22317
Javier Perez Primary <javierperezprimary@javiers-macbook-pro.local>
2018-03-19 13:19:12 -0500

    initialCommit

--- Persistence/ViewController.swift
+++ Persistence/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lineFields:[UITextField]!
+    
+    
+    func dataFileURL()-> NSURL {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        var url:NSURL?
+        url = URL(fileURLWithPath: "") as NSURL
+        
+        do{
+            try url = urls.first!.appendingPathComponent("data.plist") as NSURL
+        }catch {
+            print("Error is \(error)")
+        }
+        return url!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fileURL = self.dataFileURL()
+        if (FileManager.default.fileExists(atPath: fileURL.path!)) {
+            if let array = NSArray(contentsOf: fileURL as URL) as? [String] {
+                for i in 0..<array.count {
+                    lineFields[i].text = array[i]
+                }
+            }
+        }
+        
+        
+        let app = UIApplication.shared
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillResignActive(notification:)), name: Notification.Name.UIApplicationWillResignActive, object: app)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @objc func applicationWillResignActive(notification: NSNotification) {
+        let fileURL = self.dataFileURL()
+        let array = (self.lineFields as NSArray).value(forKey: "text") as! NSArray
+        array.write(to: fileURL as URL, atomically: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +57,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
